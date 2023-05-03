63a208b1999f159d31a8f82ce862f1b60b59767b
Andrea Cadavid <andreacadavidosorio@gmail.com>
2023-05-04 01:39:26 +1000

    Adding Subscribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
+
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import { Post } from '../post.model'
+import { PostService } from "../posts.service";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+  // posts = [
+  //   {title: "First Post", content: "This is the first post's content"},
+  //   {title: "Second Post", content: "This is the second post's content"},
+  //   {title: "Third Post", content: "This is the third post's content"},
+  // ]
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostService){ }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[])=>{
+        this.posts = posts
+      })
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.postsSub.unsubscribe();
+  }
+}
